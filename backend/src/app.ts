c901cbf9f289d8dc2168f6b2bb10e6951ebd75c1
Pathum Theekshana <kptpremawansha@students.nsbm.ac.lk>
2026-01-16 13:57:38 +0530

    created back end files add packagers

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => res.send("Backend is working!"));
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import app from "./app";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI!;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => console.error("DB connection error:", err));
